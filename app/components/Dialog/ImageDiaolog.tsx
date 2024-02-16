81276c5694c583f3981f99ab4da45ed822aecd3c
frizehero <bag4sk4r4@gmail.com>
2024-02-16 16:57:26 +0700

    mod: update preview image

--- app/components/Dialog/ImageDiaolog.tsx
+++ app/components/Dialog/ImageDiaolog.tsx
@@ -14,7 +14,7 @@ export default function ImageDialog({children, image, data, desc}: {children: Re
                 <DialogHeader>
                     {/* <DialogTitle>Are you absolutely sure?</DialogTitle> */}
                     <DialogDescription className="flex flex-col items-center relative">
-                        <img src={image} alt="image" style={{ width: "100%", transformOrigin: "0 0" }}/>
+                        <img src={image} alt="image" style={{ width: "85%", transformOrigin: "0 0" }}/>
                         <div className="absolute flex-col items-start bottom-0 w-full bg-black opacity-55 p-3">
                             <p className="text-white">{desc ?? ""}</p>
                             <p className="text-white">{data.latitude + " " + data.longitude ?? ""}</p>
